087f3ddd71fcce89786c2b51e693cc0cd05ef71d
Steven Wanderski <steven.wanderski@gmail.com>
2024-07-17 14:46:56 -0500

    Adds desktop styling

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    fontFamily: {
+      title: ['"Paytone One"'],
+      sans: ['Inter']
+    },
+    colors: {
+      green: '#4F7034',
+      white: '#FFFFFF',
+      black: '#000000'
+    },
+    backgroundImage: {
+      hero: "url('/images/hero-bg.png')"
+    },
+    extend: {},
+  },
+  plugins: [],
+}
